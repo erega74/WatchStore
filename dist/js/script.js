62663f4d9dadfde876b06a76809b613f9df817cd
Sergey Zakirov <erega74@gmail.com>
2021-07-04 17:46:28 +0500

    Добавлена страница каталога и сверстаны блоки фильтры, каталог часов, пагинация

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,4 +1,7 @@
+"use-strict"
+
 svg4everybody();
+
 
 const burgerBtn = document.querySelector(".burger-btn");
 const navLinks = document.querySelector(".header-site-nav");
@@ -24,3 +27,49 @@ const swiper = new Swiper('.swiper-container', {
     }
   },
 })
+
+const accordionButtons = document.querySelectorAll(".filters-form__button");
+
+if(accordionButtons) {
+  let accordionPanels = document.querySelectorAll(".filters-form__column");
+  for(let i = 0; i < accordionButtons.length; i++) {
+    accordionButtons[i].addEventListener("click", function(event) {
+      event.preventDefault();
+      accordionButtons[i].classList.toggle("filters-form__button--active");
+
+      let accordionPanel = accordionPanels[i];
+
+      accordionPanel.classList.toggle("filters-form__column--open");
+
+      accordionPanel.style.maxHeight = accordionPanel.style.scrollHeight + "px";
+    })
+  }
+}
+
+
+
+const rangeSlider = document.getElementById("range-slider");
+
+if(rangeSlider) {
+  noUiSlider.create(rangeSlider, {
+    start: [12000, 90000],
+    connect: true,
+    tooltips: [true,true],
+    margin: 3000,
+    padding: [12000, 10000],
+    format: wNumb({
+      decimals: 0,
+      thousand: "",
+      postfix: " ₽"
+    }),
+    range: {
+        'min': 0,
+        'max': 130000
+    }
+});
+}
+
+
+
+
+
